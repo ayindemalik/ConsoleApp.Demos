88dd650e2c13ab5fdfdfd2b2ecd700f247c1fe04
ayinde <compengmalik@gmail.com>
2025-02-14 10:38:49 +0300

    add first name and last name to the code

--- UserInputDemo/Program.cs
+++ UserInputDemo/Program.cs
@@ -1,12 +1,15 @@
 ﻿// Practice With Variables
-string name = string.Empty;
+string firstName = string.Empty;
+string lastName = string.Empty;
 int age = 0;
 
 int retirementAge = 65;
 
 // Prompt the user for input 
-Console.WriteLine("Please enter your full name");
-name = Console.ReadLine();
+Console.WriteLine("Please enter your first name");
+firstName = Console.ReadLine();
+Console.WriteLine("Please enter your last name");
+lastName = Console.ReadLine();
 
 Console.WriteLine("Please enter your age");
 age = Convert.ToInt32(Console.ReadLine());
@@ -16,6 +19,6 @@ int workingAgeRemaining = retirementAge - age;
 
 
 // Output the result
-Console.WriteLine($" Full name : {name}");
+Console.WriteLine($" Full name : {firstName} {lastName}");
 Console.WriteLine($" Age : {age}");
 Console.WriteLine($" Working years remainig is : {workingAgeRemaining}");
